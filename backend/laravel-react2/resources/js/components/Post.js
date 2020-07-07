35b949b3a6625ef7c87ada12ba141c8a5ef396ae
Merdjan Tekfidan <merttekfidan@gmail.com>
2020-07-07 13:47:35 +0200

    Revert "Initial commit"
    This reverts commit f3c301ea4a80ea8b738197a6831ecbbcb42f5e82.

--- backend/laravel-react2/resources/js/components/Post.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function Post() {
-    return (
-        <div>
-            <h1>Example content</h1>
-        </div>
-    );
-}
--- backend/laravel-react2/resources/js/components/index.js
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom";
-
-if (document.getElementById("root")) {
-    ReactDOM.render(<App />, document.getElementById("root"));
-}
