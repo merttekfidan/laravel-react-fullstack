c7365b63f7ceba8ea09f89b9dd922df4f0bbf6a3
Merdjan Tekfidan <merttekfidan@gmail.com>
2020-07-11 02:23:46 +0200

    Add update function on frontend only

--- frontend/src/components/pages/Todo.js
+++ frontend/src/components/pages/Todo.js
@@ -8,14 +8,30 @@ export default function Todo() {
       .get("http://backend.test/api/todo")
       .then((res) => setTodos(res.data));
   }
+  function flipStatus(e, i) {
+    const newTodos = [...todos];
+    if (e.target.value == 1) newTodos[i].done = 0;
+    else newTodos[i].done = 1;
+    setTodos(newTodos);
+  }
   useEffect(() => {
     getTodo();
   }, []);
   return (
     <Master>
       <ul>
-        {todos.map((todo) => (
-          <li>{todo.body}</li>
+        {console.log(todos)}
+        {todos.map((todo, i) => (
+          <li
+            value={todo.done}
+            onClick={(e) => flipStatus(e, i)}
+            style={{
+              textDecorationLine: todo.done === 1 ? "line-through" : "none",
+            }}
+            key={i}
+          >
+            {todo.body},{typeof todo}
+          </li>
         ))}
       </ul>
     </Master>
